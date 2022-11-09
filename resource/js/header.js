1979398571c36cd0ebe4b118e1edd8e191e664fb
So-Mi-Gyeong <somkung28@naver.com>
2022-11-09 17:58:51 +0900

    ajax 수정

--- resource/js/header.js
+++ resource/js/header.js
@@ -1,27 +1,30 @@
-window.onload = function() {
+$(document).ready(function(){
     $.ajax({
         type : "GET",
         url : "../incloud/header.html",
         dataType : "html",
         success: function(header_result){
             $("header").html($(header_result).filter(".wrap"));
+
+            //*********************** menu ***********************
+            const uinavs = document.querySelectorAll('.wrap .menu .ui-nav');
+
+            for(let i = 0; i < uinavs.length; i++) {
+                uinavs[i].addEventListener('click', act); //click -> act()실행
+            }
+
+            function act() {
+                this.classList.toggle('open');
+                
+                const menu = document.querySelector('.ui-menuContent');
+                menu.classList.toggle('open');
+                // const menulis = document.querySelectorAll('.menu ul li');
+                // for(let i = 0; i < menulis.length; i++) {
+                //     menulis[i].addEventListener('click', function(){
+                //         menu.classList.remove('open');
+                //     });
+                // }
+            }
         }
     });
-    //*********************** menu ***********************
-    const uinavs = document.querySelectorAll('.wrap .menu .ui-nav');
-    console.log(uinavs)
-    for(let i = 0; i < uinavs.length; i++) {
-        uinavs[i].addEventListener('click', act); //click -> act()실행
-    }
-    function act() {
-        const menu = document.querySelector('.ui-menuContent');
-        this.classList.toggle('open');
-        menu.classList.toggle('open');
-        // const menulis = document.querySelectorAll('.menu ul li');
-        // for(let i = 0; i < menulis.length; i++) {
-        //     menulis[i].addEventListener('click', function(){
-        //         menu.classList.remove('open');
-        //     });
-        // }
-    }
-}
+});
