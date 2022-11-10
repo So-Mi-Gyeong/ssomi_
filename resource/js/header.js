5a91d9362defc39a8e54c6d7c1b95b55b194a2f5
So-Mi-Gyeong <somkung28@naver.com>
2022-11-10 10:57:58 +0900

    .

--- resource/js/header.js
+++ resource/js/header.js
@@ -4,27 +4,30 @@ $(document).ready(function(){
         url : "../incloud/header.html",
         dataType : "html",
         success: function(header_result){
-            $("header").html($(header_result).filter(".wrap"));
-
-            //*********************** menu ***********************
-            const uinavs = document.querySelectorAll('.wrap .menu .ui-nav');
-
-            for(let i = 0; i < uinavs.length; i++) {
-                uinavs[i].addEventListener('click', act); //click -> act()실행
-            }
-
-            function act() {
-                this.classList.toggle('open');
-                
-                const menu = document.querySelector('.ui-menuContent');
-                menu.classList.toggle('open');
-                // const menulis = document.querySelectorAll('.menu ul li');
-                // for(let i = 0; i < menulis.length; i++) {
-                //     menulis[i].addEventListener('click', function(){
-                //         menu.classList.remove('open');
-                //     });
-                // }
-            }
+            headerNav(header_result);
         }
     });
+ 
+    function headerNav(header_result) {
+        $("header").html($(header_result).filter(".wrap"));
+         //*********************** menu ***********************
+        const uinavs = document.querySelectorAll('.wrap .menu .ui-nav');
+        for(let i = 0; i < uinavs.length; i++) {
+            uinavs[i].addEventListener('click', act); //click -> act()실행
+        }
+
+        function act() {
+            this.classList.toggle('open');
+            
+            const menu = document.querySelector('.ui-menuContent');
+            menu.classList.toggle('open');
+            // const menulis = document.querySelectorAll('.menu ul li');
+            // for(let i = 0; i < menulis.length; i++) {
+            //     menulis[i].addEventListener('click', function(){
+            //         menu.classList.remove('open');
+            //     });
+            // }
+        }
+    }
+    
 });
--- resource/js/study.js
+++ resource/js/study.js
@@ -54,24 +54,24 @@ $(document).ready(function () {
 
     /*============================================ study02 - jquery3(별점) ============================================*/
     $('.stars .fa').click(function () {
-      $(this).addClass('active')
-      $(this).prevAll().addClass('active')
-      $(this).nextAll().removeClass('active')
+      $(this).addClass('active');
+      $(this).prevAll().addClass('active');
+      $(this).nextAll().removeClass('active');
 
       var num = $(this).index()
       var starRate = num + 1
       //$('.print').text(starRate)
 
       if (starRate == 1) {
-        $('.print').html('<img src="../img/jquery/star-lv1.png">' + '별로에요')
+        $('.print').html('<img src="./resource/img/jquery/star-lv1.png">' + '별로에요')
       } else if (starRate == 2) {
-        $('.print').html('<img src="../img/jquery/star-lv2.png">' + '보통이에요')
+        $('.print').html('<img src="./resource/img/jquery/star-lv2.png">' + '보통이에요')
       } else if (starRate == 3) {
-        $('.print').html('<img src="../img/jquery/star-lv3.png">' + '그냥 그래요')
+        $('.print').html('<img src="../resource/img/jquery/star-lv3.png">' + '그냥 그래요')
       } else if (starRate == 4) {
-        $('.print').html('<img src="../img/jquery/star-lv4.png">' + '마음에 들어요')
+        $('.print').html('<img src="../resource/img/jquery/star-lv4.png">' + '마음에 들어요')
       } else {
-        $('.print').html('<img src="../img/jquery/star-lv5.png">' + '아주 좋아요')
+        $('.print').html('<img src="../resource/img/jquery/star-lv5.png">' + '아주 좋아요')
       }
     });
 
