a63966bcae144046bb018ba2a58f9d4b724a1a3b
So-Mi-Gyeong <somkung28@naver.com>
2022-11-23 16:32:47 +0900

    chart

--- resource/js/ui-script.js
+++ resource/js/ui-script.js
@@ -1,4 +1,83 @@
 $(document).ready(function(){
+    //*********************** chart(chart.html) ***********************
+    //chart01
+    var options = {
+        animationEnabled: true,
+        title: {
+            text: "Mobile Phones Used For",                
+            fontColor: "Peru"
+        },	
+        axisY: {
+            tickThickness: 0,
+            lineThickness: 0,
+            valueFormatString: " ",
+            includeZero: true,
+            gridThickness: 0                    
+        },
+        axisX: {
+            tickThickness: 0,
+            lineThickness: 0,
+            labelFontSize: 18,
+            labelFontColor: "Peru"				
+        },
+        data: [{
+            indexLabelFontSize: 26,
+            toolTipContent: "<span style=\"color:#62C9C3\">{indexLabel}:</span> <span style=\"color:#CD853F\"><strong>{y}</strong></span>",
+            indexLabelPlacement: "inside",
+            indexLabelFontColor: "white",
+            indexLabelFontWeight: 600,
+            indexLabelFontFamily: "Verdana",
+            color: "#62C9C3",
+            type: "bar",
+            dataPoints: [
+                { y: 21, label: "21%", indexLabel: "Video" },
+                { y: 25, label: "25%", indexLabel: "Dining" },
+                { y: 33, label: "33%", indexLabel: "Entertainment" },
+                { y: 36, label: "36%", indexLabel: "News" },
+                { y: 42, label: "42%", indexLabel: "Music" },
+                { y: 49, label: "49%", indexLabel: "Social Networking" },
+                { y: 50, label: "50%", indexLabel: "Maps/ Search" },
+                { y: 55, label: "55%", indexLabel: "Weather" },
+                { y: 61, label: "61%", indexLabel: "Games" }
+            ]
+        }]
+    };
+    
+    $("#chartContainer").CanvasJSChart(options);
+
+    //chart02
+    var replayBtn = document.querySelector('.replay');
+
+    draw(80, '.pie-chart1', '#de586d');
+    draw(50, '.pie-chart2', '#a2b9ee');
+    draw(30, '.pie-chart3', '#f8d49b');
+
+    function draw(max, classname, colorname){
+      var i=1;
+        var func1 = setInterval(function(){
+        if(i<max){
+          color1(i,classname,colorname);
+          i++;
+        } else{
+          clearInterval(func1);
+        }
+        },10);
+    }
+    function color1(i, classname,colorname){
+      $(classname).css({
+          // "background":"conic-gradient(" + colorname +" 0%, #f2a490 "+ i +"%, #1c2135 0%, #000 100%)"
+          "background":"conic-gradient(#f2a490 0%," + colorname + " " + i +"%, #1c2135 0%, #000 100%)"
+      });
+    }
+  
+    function replay(){
+      draw(80, '.pie-chart1', '#de586d');
+      draw(50, '.pie-chart2', '#a2b9ee');
+      draw(30, '.pie-chart3', '#f8d49b');
+    }
+
+    replayBtn.addEventListener("click", replay);
+
     //*********************** input script(inp_acco.html) ***********************
     const all = document.querySelector('#chkAll');
     var inp = document.getElementsByName('chk');
