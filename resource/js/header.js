2afec13b8dbf26a27593003a8967edb002a406a2
So-Mi-Gyeong <somkung28@naver.com>
2023-05-19 17:47:49 +0900

    경로수정

--- resource/js/header.js
+++ resource/js/header.js
@@ -8,6 +8,14 @@ $(document).ready(function(){
             headerNav(header_result);
         }
     });
+    $.ajax({
+      type : "GET",
+      url : "../includ/header.html",
+      dataType : "html",
+      success: function(header_result){
+          headerNav(header_result);
+      }
+  });
  
     function headerNav(header_result) {
         $("#menu").html($(header_result).filter(".wrap"));
