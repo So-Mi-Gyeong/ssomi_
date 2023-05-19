c33dfaf0fd6746024d101178a39de85fb3fe0eec
So-Mi-Gyeong <somkung28@naver.com>
2023-05-19 15:30:49 +0900

    경로수정

--- guide/resource/js/header.js
+++ guide/resource/js/header.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.ajax({
         type : "GET",
-        url : "/includ/header.html",
+        url : "./includ/header.html",
         dataType : "html",
         success: function(header_result){
             headerNav(header_result);
